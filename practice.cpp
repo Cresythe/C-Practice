c907ea82952616a2cd48dc6dad528ede8674e93c
Cresythe <106111373+Cresythe@users.noreply.github.com>
2024-07-19 19:27:21 -0400

    Covered most of String methods of C++

--- practice.cpp
+++ practice.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 // using namespace std;
-
+/*
+All code within this file was made without using the namespace std, so if things look confusing that is why.
+*/
 //Practice area 
 int main()
 {
@@ -43,6 +45,83 @@ int main()
     // std::cin >> num1;
     // std::cout << "Type in the second number: "; 
     // std::cin >> num2;     
+
+    // //String Concatenation 
+    // std::string name1 = "Bob "; 
+    // std::string name2 = "the Builder";
+    // std::string fullName;
+
+    //-Method 1
+    // fullName = name1 + name2;
+
+    //-Method 2, Append
+    // fullName = name1.append(name2);
+    // std::cout << fullName;
+
+    /*
+    String Methods
+    -length()
+    -size()
+    Both do the same thing in showing the length of a string
+    */
+
+    //std::string words = "Help me, help you"; 
+    //std::cout << "String length: " << words.length() << "\n"; 
+    //std::cout << "String length: " << words.size(); 
+
+    /*
+    Accessing Strings 
+    -Using [] alongside variables allows for specified character
+    -Seems like strings are mutable so allows for
+        ~ string word = "Hello"
+        ~ word[0] = 'j';
+        ~ std::cout << word //prints out jello 
+    */
+   //std::string worded = "Hello World"; 
+   //std::cout << worded[2] << std::endl; //Prints l 
+   //worded[6] = 'M'; 
+   //std::cout << worded << "\n"; //prints out "Hello Morld"
+
+   //std::cout << worded.at(0); // does the same thing as using []
+    
+    /*
+    Escape characters 
+    -\' ~ Single qoute
+    -\" ~ Double qoute 
+    -\\ ~ Backslash
+    -\n ~ New Line
+    -\t ~ Tab
+    */
+    
+   /*
+   Inputs via String
+   -Can use the normal cin >> variable to take a string
+   -but cin sees "spaces" as a terminating character so single inputs is the only thing cin can take 
+   -the "getLine()" function is normally used for String input
+   ~ examples below
+   */
+
+    // std::string fullName; 
+    // std::cout << "Type in your full name: "; 
+    // std::cin >> fullName; 
+    // std::cout << "Your name is " + fullName; //only prints out the first word due to the space
+
+    //Using getLine() instead
+    // std::cout << "Type in your full name: "; 
+    // std::getline(std::cin, fullName);
+    // std::cout << "Your name is: " + fullName;  
+
+    /*
+    C-Style String
+    -Within the C langauge since it didn't support string type
+    -To make a string, "char" and a array of characters were used to make strings in C.
+    The only reason to use this style is to use CString functions
+    ~ example below ~ 
+    */
+    // std::string greeting1 = "hello"; //Regular string
+    // char greeting2[] = "Hello"; //C-style String (Array of characters)
+
+    
     return 0;
 }
 
