2937983b794e8cb92925f2668f991fedb6c1feaf
Cresythe <106111373+Cresythe@users.noreply.github.com>
2024-07-19 18:11:09 -0400

    learned:  datatypes, string functions, input/output

--- practice.cpp
+++ practice.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
+#include <string>
 // using namespace std;
 
+//Practice area 
 int main()
 {
-    std::cout << "Hello Corey" << std::endl;
-    std::cout << "Hello C++";
+    // int num = 35; 
+    // std::cout << "Hello Corey" << std::endl;
+    // std::cout << "Hello C++" << std::endl;
+    // // std::cout << num; 
+    // std::cout << "Today is the " << num << "th day of the month" << std::endl;
+
+
+    // // int x = 5; 
+    // // int y = 20; 
+    // // std::cout << x + y; 
+
+    // //Multi-declaration of variables
+    // std::string word = "hello", word1 = "World"; 
+    // std::cout << word + " " + word1 + "\n";
+
+    // //With numbers 
+    // int x , y , z; 
+    // x = y = z = 50; 
+    // std::cout << x + y + z; 
+
+    // //input and output
+    // int num; 
+    // std::cout << "Type a number: "; 
+    // std::cin >> num; 
+    // std::cout << "your number is " << num; 
+
+
+    // // boolean operators 
+    // std::cout << (3 > 5) << std::endl; 
+    // std::cout << (5 < 4) << std::endl;
+
+    // // boolean game -- will continue once I learn conditionals 
+    // int num1; 
+    // int num2; 
+    // std::cout << "Type the first number: "; 
+    // std::cin >> num1;
+    // std::cout << "Type in the second number: "; 
+    // std::cin >> num2;     
     return 0;
 }
+
+//datatypes 
+/*
+int num = 24; 
+double nums = 24.5; 
+char letter = 'A'; 
+string word = "Hello World"; 
+bool value = false; 
+float floNum = 3.14; 
+float floNum = 6.206e24; 
+*/ 
